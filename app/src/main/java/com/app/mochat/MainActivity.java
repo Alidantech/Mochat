10a032575f34e620072ad50f9f288b3f3d33ab50
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-08 19:37:41 +0300

    prototype one

--- app/src/main/java/com/app/mochat/MainActivity.java
+++ app/src/main/java/com/app/mochat/MainActivity.java
@@ -3,8 +3,13 @@ package com.app.mochat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.view.View.OnClickListener;
+
 
 public class MainActivity extends AppCompatActivity {
+    FrameLayout frameLayout = findViewById(R.id.frame_1);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
