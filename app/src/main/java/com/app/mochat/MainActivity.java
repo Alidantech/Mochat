c2a22cc371e60b20c4507510304541ac69f78712
PETER IRUNGU <peteralidante254@gmail.com>
2023-02-09 01:05:42 +0300

    Adding Fragments

--- app/src/main/java/com/app/mochat/MainActivity.java
+++ app/src/main/java/com/app/mochat/MainActivity.java
@@ -1,19 +1,31 @@
 package com.app.mochat;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.view.View.OnClickListener;
-
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    FrameLayout frameLayout = findViewById(R.id.frame_1);
-
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnChat = findViewById(R.id.button_open);
+        btnChat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                     fragmentManager.beginTransaction()
+                             .replace(R.id.frame_1, ChatFragment.class, null)
+                             .setReorderingAllowed(true)
+                             .addToBackStack("name")
+                             .commit();
+            }
+        });
     }
 }
